b052c33360d2df94e984fe3edf2933cdde87f731
rtrt1594 <rtrt1594@naver.com>
2024-06-25 19:00:44 +0900

    Title: 정수 선언하고 뺄셈, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240625/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int s;
+        s = 97;
+
+        int a;
+        a = 13;
+        
+        int b = s - a;  
+        System.out.println(s + " - " + a +" = 84");
+    }
+}
