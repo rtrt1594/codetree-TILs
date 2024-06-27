22c93291f08e93f89f673b0dfeb5d491cb3e8ed1
rtrt1594 <rtrt1594@naver.com>
2024-06-27 18:30:39 +0900

    Title: 길이 단위 변환하기, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240627/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 9.2;
+        double b = 30.48;
+        double c = 1.3;
+        int d = 160934;
+        double a1 = a*b;
+        double a2 = c*d;
+        System.out.printf(a + "ft" + " = " + "%.1f" + "cm",a1);
+        System.out.println();
+        System.out.printf(c + "mi" + " = " + "%.1f" + "cm",a2);
+    }
+}
