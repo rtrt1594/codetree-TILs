e6f71146e3be0e08d073b13da7164503b24ce5de
rtrt1594 <rtrt1594@naver.com>
2024-06-28 18:13:02 +0900

    Title: 데이터 교환, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240628/데이터 교환/exchange-data.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp = a;
+        a = c;
+        c = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
