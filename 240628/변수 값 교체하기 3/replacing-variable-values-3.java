831b143a04c21d2d55d37ce05d8556efc4676655
rtrt1594 <rtrt1594@naver.com>
2024-06-28 17:45:40 +0900

    Title: 변수 값 교체하기 3, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240628/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int temp;
+        temp = a;
+        a = b;
+        a = 5;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
